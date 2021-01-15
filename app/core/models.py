f40fc4bd271260d5ca75add33dd70513026d868f
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2021-01-15 18:12:50 -0300

    added User model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,35 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email: str, name: str, password: str, bio: str = None, **extra_fields):
+        if not email:
+            raise ValueError(_('E-mail address is required'))
+        if not name:
+            raise ValueError(_('Name is required'))
+        if not password:
+            raise ValueError(_('Password is required'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, bio=bio, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+
+class User(AbstractBaseUser):
+    """User model, used for authentication"""
+    email = models.EmailField(unique=True) # Required
+    name = models.CharField(max_length=255) # Required
+    bio = models.CharField(max_length=360)
+    profile_picture = models.ImageField()
+
+    EMAIl_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'email', 'password']
+
+    objects = UserManager()
